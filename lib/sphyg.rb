d40ad18ad228a5c9a7dcc3d34003489a40f45cc1
Shane Cavanaugh <shane@shanecav.net>
2017-12-30 11:18:15 -0500

    Some rdoc-style documentation for the convenience method

--- lib/sphyg.rb
+++ lib/sphyg.rb
@@ -2,6 +2,14 @@ require 'sphyg/pulse'
 require 'sphyg/version'
 
 module Sphyg
+  # Presents a message and throbber
+  # Params:
+  # +message+:: friendly +string+ to display that indicates a command is running
+  #   for example, 'Please wait'
+  # +options+:: configuration +hash+. Currently only uses the +:kind+ option to
+  #   configure which throbber to use. Defaults to +{ kind: :wave }+
+  # +&blk+:: a +block+ containing a long-running command to run while displaying
+  #   the message and throbber
   def self.pulse(message, options = nil, &blk)
     ::Sphyg::Pulse.new(message, options).run { yield blk }
   end
