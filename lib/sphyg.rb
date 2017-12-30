5d60a8b9d3bda0a7d7bfbd54c505a336409713a3
Shane Cavanaugh <shane@shanecav.net>
2017-12-30 00:43:44 -0500

    Don't require options hash in convenience method

--- lib/sphyg.rb
+++ lib/sphyg.rb
@@ -2,7 +2,7 @@ require "sphyg/pulse"
 require "sphyg/version"
 
 module Sphyg
-  def self.pulse(message, options, &blk)
+  def self.pulse(message, options = nil, &blk)
     ::Sphyg::Pulse.new(message, options).run { yield blk }
   end
 end
