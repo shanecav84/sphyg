e3ea8a7bebba1a2b7dab9c01f3d97bcbc66abeb6
Shane Cavanaugh <shane@shanecav.net>
2017-12-30 10:49:48 -0500

    Run a test for each throbber kind

--- spec/sphyg/pulse_spec.rb
+++ spec/sphyg/pulse_spec.rb
@@ -4,11 +4,11 @@ require 'sphyg/pulse'
 
 module Sphyg
   RSpec.describe Pulse do
-    it "doesn't raise an error" do
-      Sphyg::FRAMES.each_key do |kind|
-        message = "Testing #{kind}"
-        expect { described_class.new(message, { kind: kind}).run { sleep 1 } }.
-          to_not raise_error
+    Sphyg::FRAMES.each_key do |kind|
+      it "using :#{kind} doesn't raise an error" do
+          message = "Testing #{kind}"
+          expect { described_class.new(message, { kind: kind }).run { sleep 1 } }.
+            to_not raise_error
       end
     end
   end
