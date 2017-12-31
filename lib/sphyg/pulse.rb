299d9357c29f72e204b9f76ff384ea1e26f315d4
Shane Cavanaugh <shane@shanecav.net>
2017-12-31 18:15:09 -0500

    Remove Thread config and add note about our thread's exceptions

--- lib/sphyg/pulse.rb
+++ lib/sphyg/pulse.rb
@@ -9,6 +9,9 @@ module Sphyg
     end
 
     def run(&blk)
+      # Note: the block will continue to run even if our throbber crashed. This
+      # is more user-frindly than setting `abort_on_exception` for our thread.
+      # TODO: use `Thread#reporting_on_exception` for Ruby versions >= 2.4
       thr = ::Thread.new { ::Sphyg::Throbber.new(@message, @options[:kind]).run }
       yield blk
     ensure
