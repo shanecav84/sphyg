2278914f48faa6b7417f693a6695b9228a5aa900
Shane Cavanaugh <shane@shanecav.net>
2017-12-31 13:52:16 -0500

    Linting

--- spec/sphyg_spec.rb
+++ spec/sphyg_spec.rb
@@ -17,15 +17,15 @@ RSpec.describe Sphyg do
       described_class.pulse(message, options) { sleep 1 }
     end
 
-    describe "options argument" do
+    describe 'options argument' do
       it "doesn't crash when passed nil" do
         message = 'Please wait'
         mock_pulse = double('Sphyg::Pulse')
 
         expect(::Sphyg::Pulse).
           to receive(:new).
-            with(message, nil).
-            and_return(mock_pulse)
+          with(message, nil).
+          and_return(mock_pulse)
         expect(mock_pulse).to receive(:run).and_yield
 
         described_class.pulse(message) { sleep 1 }
@@ -37,8 +37,8 @@ RSpec.describe Sphyg do
 
         expect(::Sphyg::Pulse).
           to receive(:new).
-            with(message, {}).
-            and_return(mock_pulse)
+          with(message, {}).
+          and_return(mock_pulse)
         expect(mock_pulse).to receive(:run).and_yield
 
         described_class.pulse(message, {}) { sleep 1 }
