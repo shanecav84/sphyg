9a9929d7a972fcf6ee2e09e4ccbfd7be1e69b334
Shane Cavanaugh <shane@shanecav.net>
2017-12-31 20:17:36 -0500

    Use stdlib method

--- lib/sphyg/strategies/wave.rb
+++ lib/sphyg/strategies/wave.rb
@@ -5,21 +5,12 @@ module Sphyg
     # Cyclically shifts frames along sequence
     class Wave < Base
       def run
-        frames_index = 0
         current_frames = frames.dup
         loop do
           print_message_and_pulser(current_frames.join)
-          current_frames << cycle_frames(current_frames, frames_index)
+          current_frames.rotate!
           sleep pulse_rate
         end
-      end
-
-      private
-
-      def cycle_frames(current_frames, current_frames_index)
-        current_frames.shift
-        new_frames_index = (current_frames_index + 1) % current_frames.length
-        current_frames[new_frames_index]
       end
     end
   end
