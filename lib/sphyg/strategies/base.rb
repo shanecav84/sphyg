b87f7375c15ad6c62720fd2c26f49c687ef721e1
Shane Cavanaugh <shane@shanecav.net>
2017-12-31 21:40:09 -0500

    Raise error if abstract method called

--- lib/sphyg/strategies/base.rb
+++ lib/sphyg/strategies/base.rb
@@ -2,13 +2,16 @@ module Sphyg
   module Strategies
     # Strategy template
     class Base
+      class AbstractMethodCall < StandardError; end
       def initialize(message, frames, pulse_rate)
         @message = message
         @frames = frames
         @pulse_rate = pulse_rate
       end
 
-      def run; end
+      def run
+        raise AbstractMethodCall, 'Implement :run in a child class'
+      end
     end
   end
 end
