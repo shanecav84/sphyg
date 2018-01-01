72653b6f6d06cb6be0c14f3efc60b69c5054cef2
Shane Cavanaugh <shane@shanecav.net>
2017-12-31 22:29:16 -0500

    Suppress output stdout in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,9 @@ Thread.abort_on_exception = true
 RSpec.configure do |config|
   config.order = :random
   Kernel.srand config.seed
+
+  config.before(:each) do
+    allow_any_instance_of(::Sphyg::Pulse).to receive(:print).with("\n")
+    allow_any_instance_of(::Sphyg::Throbber).to receive(:print)
+  end
 end
